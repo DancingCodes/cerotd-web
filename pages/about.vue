7daddfebc0d1879bfbeabc01c9a407e924e30628
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-09-23 15:10:55 +0800

    1

--- pages/about.vue
+++ pages/about.vue
@@ -16,7 +16,7 @@
         <div class="intro-panel">
           <video
             class="intro-panel-video"
-            src="/factory.mp4"
+            src="https://cdn.moonc.love/files/2026-09/0801a9d1-c139-49ca-92b8-16a1ce7514e8.mp4"
             autoplay
             muted
             loop
