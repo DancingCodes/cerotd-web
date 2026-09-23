99d68f3f036bfa3e1ed8d6aaed027ada5fb125b7
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-09-23 12:45:03 +0800

    1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-01-01',
   modules: ['@nuxtjs/i18n'],
-  css: ['~/assets/styles/main.scss'],
+  css: ['modern-normalize/modern-normalize.css', '~/assets/styles/main.scss', '~/assets/styles/common.scss'],
   typescript: {
     typeCheck: false,
     strict: true
