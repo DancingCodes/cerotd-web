881d4154b1a86b5e2a67655e0af5a06af20e7739
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-09-23 16:49:27 +0800

    1

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="layout-default">
+    <Header />
+    <div class="layout-default-main">
+      <slot />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout-default {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+
+  .layout-default-main {
+    flex: 1;
+  }
+}
+</style>
